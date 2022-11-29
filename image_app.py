42d019ad856c4d7877bac0185fc5cff570fd3a76
patrick-naylor <patrickknaylor@gmail.com>
2022-11-28 16:38:12 -0800

    Updated image_app.py to include refresh system

--- image_app.py
+++ image_app.py
@@ -4,6 +4,7 @@ import cv2
 import glob
 import time
 import os
+save_path = 'Images/Masked/'
 
 
 
@@ -13,10 +14,10 @@ def click_event(event, x, y, flags, params):
         
         if len(clicks) < 4:
             clicks.append([x,y])
-            print(clicks)
+            #print(clicks)
         # displaying the coordinates
         # on the Shell
-        print(x, ' ', y)
+        #print(x, ' ', y)
  
         # displaying the coordinates
         # on the image window
@@ -32,30 +33,42 @@ def click_event(event, x, y, flags, params):
  
     # checking for right mouse clicks    
 
-
-
-
+    
 
 if __name__ == '__main__':
-    clicks = []
-    raw_paths = glob.glob('Images/Raw/*')
-    print(raw_paths)
-    img = cv2.imread(raw_paths[0])
-    h, w, c = img.shape
-    
-    cv2.putText(img, 'Place four corners', (w//2, h//2),cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2)
-    cv2.imshow('image', img)
-    cv2.setMouseCallback('image', click_event)
+    bool = True
+    for i in range(1000000):
+        print(i)
+        clicks = []
+        raw_paths = glob.glob('Images/Raw/*')
+        path = raw_paths[0]
+        file_label = path[11:-4]
+        #print(path[10:-3])
+        img = cv2.imread(path)
+        h, w, c = img.shape
+        mask_arr = np.zeros((h, w))
+        
+        cv2.putText(img, 'Place four corners', (w//2, h//2),cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2)
+        cv2.imshow('image', img)
+        cv2.setMouseCallback('image', click_event)
+        while bool:
+            k = cv2.waitKey(0)
+            #print(k)
+            if k == 27:
+                bool = False
+            elif (k == ord('s')) and (len(clicks)  == 4):
+                #print('save here')
+                os.rename(path, f'{save_path}{file_label}.jpg')
+                break
+            elif (k == ord('l')):
+                #print('remove picture')
+                os.remove(path)
+                break
 
-    while True:
-        k = cv2.waitKey(0)
-        print(k)
-        if k == 27:
-            cv2.destroyAllWindows()
+            elif (k == 127):
+                clicks = []
+                img = cv2.imread(raw_paths[0])
+                cv2.imshow('image', img)
+        if not bool:
             break
-        elif (k == ord('s')) and (len(clicks) == 4):
-            print('save here')
-        elif (k == 127):
-            clicks = []
-            img = cv2.imread(raw_paths[0])
-            cv2.imshow('image', img)
+    cv2.destroyAllWindows
