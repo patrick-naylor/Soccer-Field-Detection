808240a2188b03f36bef4b9f55acb76693b1a654
patrick-naylor <patrickknaylor@gmail.com>
2022-12-01 14:43:53 -0800

    Updated file_adjustment.py

--- file_adjustment.py
+++ file_adjustment.py
@@ -1,2 +1,7 @@
 import numpy as np
 import glob
+import os
+files = glob.glob('/Users/patricknaylor/Desktop/Field_Detection/Images/Masked/*.csv')
+for file in files:
+    arr = np.loadtxt(file, deliiter=',', dtype='int8')
+    name = file[:-4]
