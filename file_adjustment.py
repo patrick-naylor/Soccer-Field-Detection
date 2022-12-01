47eba1595c06233e1eee3d8ab59557b961940048
patrick-naylor <patrickknaylor@gmail.com>
2022-12-01 14:46:45 -0800

    Fixed bugs

--- file_adjustment.py
+++ file_adjustment.py
@@ -3,7 +3,7 @@ import glob
 import os
 files = glob.glob('/Users/patricknaylor/Desktop/Field_Detection/Images/Masked/*.csv')
 for file in files:
-    arr = np.loadtxt(file, deliiter=',', dtype='int8')
+    arr = np.loadtxt(file, delimiter=',', dtype='int8')
     name = file[:-4]+'.npy'
     np.save(name, arr)
     os.remove(file)
