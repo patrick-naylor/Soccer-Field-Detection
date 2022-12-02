0877a015a920e9ad9bb18dcf0c2392a1ba8e8a6f
patrick-naylor <patrickknaylor@gmail.com>
2022-12-01 20:48:10 -0800

    Black formatted file_adjustment.py

--- file_adjustment.py
+++ file_adjustment.py
@@ -1,9 +1,10 @@
 import numpy as np
 import glob
 import os
-files = glob.glob('/Users/patricknaylor/Desktop/Field_Detection/Images/Masked/*.csv')
+
+files = glob.glob("/Users/patricknaylor/Desktop/Field_Detection/Images/Masked/*.csv")
 for file in files:
-    arr = np.loadtxt(file, delimiter=',', dtype='int8')
-    name = file[:-4]+'.npy'
+    arr = np.loadtxt(file, delimiter=",", dtype="int8")
+    name = file[:-4] + ".npy"
     np.save(name, arr)
     os.remove(file)
