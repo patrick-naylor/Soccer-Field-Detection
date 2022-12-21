73c2f4685515b28dbceb943737140048c387be71
patrick-naylor <patrickknaylor@gmail.com>
2022-12-21 15:26:03 -0800

    Mask Applied

--- soccerdetect.py
+++ soccerdetect.py
@@ -4,9 +4,28 @@ import torchvision.transforms as transforms
 import torch.nn.functional as F
 import torch.nn as nn
 import glob
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+from scipy import interpolate
 
 
-model_path = 'model_latest.pth'
+model_path = '/Users/patricknaylor/Desktop/Field_Detection/data/Models/model_latest.pth'
+class imageDataset(Dataset):
+    def __init__(self, X):
+        self.X = X
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index):
+        image = self.X[index]
+        X = self.transform(image)
+        return [X]
+
+    transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ToTensor()])
+
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
@@ -29,21 +48,39 @@ class Model(nn.Module):
 model = Model()
 
 def field_mask(image):
-    image_shrunk = cv2.resize(image, (128, 72), interpolation=cv2.INTER_AREA)
-    im_transforms = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.ToTensor()
-    ])
-    image_shrunk = im_transforms(image_shrunk)
-    model.load_state_dict(torch.load(model_path))
-    mask = model(image_shrunk)
-    mask = mask.reshapel(36, 64)
-    print(mask.size(), image_shrunk.size())
+    with torch.no_grad():
+        image_shrunk = cv2.resize(image, (128, 72), interpolation=cv2.INTER_AREA)
+        print(image_shrunk.shape)
+        image_shrunk = np.expand_dims(image_shrunk, 0)
+        image_ds = imageDataset(X=image_shrunk)
+        image_dl = DataLoader(image_ds, 1)
+        model.load_state_dict(torch.load(model_path))
+        for input in image_dl:
+            mask = model(input[0])
+        mask = mask.reshape(36, 64)
+        mask_detach = mask.numpy()
+        x = np.linspace(0, 1, mask_detach.shape[0])
+        y = np.linspace(0, 1, mask_detach.shape[1])
+        f = interpolate.interp2d(y, x, mask_detach, kind='linear')
+
+        x2 = np.linspace(0, 1, 720)
+        y2 = np.linspace(0, 1, 1280)
+        mask_expand = f(y2, x2)
+        print(mask_expand.shape)
+        print(mask_expand)
+        masked_image = np.zeros((720, 1280, 3))
+        for i in range(3):
+            masked_image[:, :, i] = mask_expand * image[:, :, i]
+
+        return masked_image
 
 #TESTING
 image_path = '/Users/patricknaylor/Desktop/Field_Detection/Images/Masked/'
 images = list(glob.glob(image_path + '*.jpg'))
 test_image_path = images[0]
 test_image = cv2.cvtColor(cv2.imread(test_image_path), cv2.COLOR_BGR2RGB)
-field_mask(test_image)
+masked_image = field_mask(test_image)
+#cv2.imshow('image', masked_image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows
 #TESTINGJ
